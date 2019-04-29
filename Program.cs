7959a462bab36f7e0e96c72644c1b3db56be36d1
Pliyo San <pliyosan@gmail.com>
2019-04-29 14:05:02 +0100

    Adding Program.cs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.Azure.ServiceBus;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order() { Id = 1 };
+
+            var serviceBusMessage = InitMessage(order);
+
+            // Do something with it.
+
+        }
+
+        private static Message InitMessage<T>(T message)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            var xml = "";
+            using(var stringWriter = new StringWriter())
+            {
+                using(XmlWriter writer = XmlWriter.Create(stringWriter))
+                {
+                    serializer.Serialize(writer, message);
+                    xml = stringWriter.ToString(); // Your XML
+                }
+            }
+
+            var xmlInBytes = Encoding.UTF8.GetBytes(xml);
+            return new Message(xmlInBytes);
+        }
+    }
+
+    public class Order 
+    {
+        public int Id { get; set; }
+    }
+}
